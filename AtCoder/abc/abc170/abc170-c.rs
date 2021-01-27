e2ea459e75861f5f2637928dd1e85544220e2700
Acasune <tttt4aaaa@gmail.com>
2021-01-27 21:49:15 +0900

    [Solve] abc170-c

--- AtCoder/abc/abc170/abc170-c.rs
+++ AtCoder/abc/abc170/abc170-c.rs
@@ -1,3 +1,78 @@
+use std::{cmp::min, io::*, str::FromStr};
+
+#[allow(unused_macros)]
+macro_rules! scan {
+  ($e:expr; $t:ty) => {
+    $e.get::<$t>()
+  };
+  ($e:expr; $($t:ty), *) => {
+    ($($e.get::<$t>(),)*)
+  }
+}
+
+struct Scanner<R: BufRead> {
+    reader: R,
+    iter: std::vec::IntoIter<String>,
+}
+
+#[allow(dead_code)]
+impl<R: BufRead> Scanner<R> {
+    fn new(reader: R) -> Scanner<R> {
+        Scanner {
+            reader,
+            iter: vec![String::new()].into_iter(),
+        }
+    }
+    fn new_line(&mut self) {
+        let mut line = String::new();
+        self.reader.read_line(&mut line).ok();
+        self.iter = line
+            .trim()
+            .split_whitespace()
+            .map(|s| s.to_string())
+            .collect::<Vec<String>>()
+            .into_iter();
+    }
+    fn get<T: FromStr>(&mut self) -> T {
+        self.iter.next().unwrap().parse().ok().unwrap()
+    }
+    fn get_as_vec<T: FromStr>(&mut self) -> Vec<T> {
+        self.iter.clone().map(|v| v.parse().ok().unwrap()).collect()
+    }
+    fn get_line(&mut self) -> String {
+        let mut line = String::new();
+        self.reader.read_line(&mut line).ok();
+        line.trim().to_string()
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cin = stdin();
+    let cin = cin.lock();
+    let mut sc = Scanner::new(cin);
+
+    sc.new_line();
+    let x:i64 =sc.get();
+    let n:i64 =sc.get();
+    if n ==0 {
+        println!("{}",x);
+        return ;
+    }
+    sc.new_line();
+    let mut v:Vec<usize> = sc.get_as_vec();
+    let mut v2= vec![0;102];
+    for e in v {
+        v2[e as usize] =1;
+    }
+    let mut ans:i64 =1_000_000_008;
+    let mut cmp:i64 = 1_000_000_008;
+    for i in 0..v2.len(){
+        if v2[i] == 0 && (x - i as i64).abs() < cmp{
+            ans = i as i64;
+            cmp = (x -i as i64).abs();
+        }
+
+    }
+    println!("{}",ans);
+
 }
