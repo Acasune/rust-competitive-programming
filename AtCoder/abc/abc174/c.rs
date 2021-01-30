11bda46796503b9ae130e4d93bcda0027ae8356d
Acasune <tttt4aaaa@gmail.com>
2021-01-30 16:47:34 +0900

    Solve: abc174-c

---
+++ AtCoder/abc/abc174/c.rs
@@ -0,0 +1,71 @@
+use std::{cmp::{max, min}, collections::VecDeque, io::*, str::{Chars, FromStr}};
+
+#[allow(unused_macros)]
+macro_rules! scan {
+  ($e:expr; $t:ty) => {
+    $e.get::<$t>()
+  };
+  ($e:expr; $($t:ty), *) => {
+    ($($e.get::<$t>(),)*)
+  }
+}
+
+struct Scanner<R: BufRead> {
+    reader: R,
+    iter: std::vec::IntoIter<String>,
+}
+
+#[allow(dead_code)]
+impl<R: BufRead> Scanner<R> {
+    fn new(reader: R) -> Scanner<R> {
+        Scanner {
+            reader,
+            iter: vec![String::new()].into_iter(),
+        }
+    }
+    fn new_line(&mut self) {
+        let mut line = String::new();
+        self.reader.read_line(&mut line).ok();
+        self.iter = line
+            .trim()
+            .split_whitespace()
+            .map(|s| s.to_string())
+            .collect::<Vec<String>>()
+            .into_iter();
+    }
+    fn get<T: FromStr>(&mut self) -> T {
+        self.iter.next().unwrap().parse().ok().unwrap()
+    }
+    fn get_as_vec<T: FromStr>(&mut self) -> Vec<T> {
+        self.iter.clone().map(|v| v.parse().ok().unwrap()).collect()
+    }
+    fn get_line(&mut self) -> String {
+        let mut line = String::new();
+        self.reader.read_line(&mut line).ok();
+        line.trim().to_string()
+    }
+}
+
+fn main() {
+    let cin = stdin();
+    let cin = cin.lock();
+    let mut sc = Scanner::new(cin);
+
+    sc.new_line();
+    let mut k:usize =sc.get();
+
+    let mut v = vec![0;k+3];
+
+    v[1] = 7%k;
+    for i in 2..=k{
+        v[i] =(10*v[i-1] +7)%k;
+    }
+    for i in 1..=k{
+        if v[i] == 0{
+            println!("{}",i);
+            return ;
+        }
+    }
+    println!("{}", -1);
+
+}
