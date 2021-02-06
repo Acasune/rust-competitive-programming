3842d64d47b71bf6153174af6661a6661b731cae
Acasune <tttt4aaaa@gmail.com>
2021-02-06 15:47:02 +0900

    Create: a directory for fine selections

---
+++ fine-selections-of-past-problems/selections-for-intermediate-contestants/05-binary-search/08-ALDS1-4-B.rs
@@ -0,0 +1,78 @@
+﻿use std::{io::*, str::FromStr};
+
+#[allow(unused_macros)]
+macro_rules! scan {
+  ($e:expr; $t:ty) => {
+    $e.get::<$t>()
+  };
+  ($e:expr; $($t:ty), *) => {
+    ($($e.get::<$t>(),)*)
+  }
+}
+
+struct Scanner<R: BufRead> {
+    reader: R,
+    iter: std::vec::IntoIter<String>,
+}
+
+#[allow(dead_code)]
+impl<R: BufRead> Scanner<R> {
+    fn new(reader: R) -> Scanner<R> {
+        Scanner {
+            reader,
+            iter: vec![String::new()].into_iter(),
+        }
+    }
+    fn new_line(&mut self) {
+        let mut line = String::new();
+        self.reader.read_line(&mut line).ok();
+        self.iter = line
+            .trim()
+            .split_whitespace()
+            .map(|s| s.to_string())
+            .collect::<Vec<String>>()
+            .into_iter();
+    }
+    fn get<T: FromStr>(&mut self) -> T {
+        self.iter.next().unwrap().parse().ok().unwrap()
+    }
+    fn get_as_vec<T: FromStr>(&mut self) -> Vec<T> {
+        self.iter.clone().map(|v| v.parse().ok().unwrap()).collect()
+    }
+    fn get_line(&mut self) -> String {
+        let mut line = String::new();
+        self.reader.read_line(&mut line).ok();
+        line.trim().to_string()
+    }
+}
+
+fn main() {
+    let cin = stdin();
+    let cin = cin.lock();
+    let mut sc = Scanner::new(cin);
+
+    sc.new_line();
+    sc.new_line();
+    let s: Vec<i64> = sc.get_as_vec();
+
+    sc.new_line();
+    sc.new_line();
+    let t: Vec<i64> = sc.get_as_vec();
+
+    let mut ans = 0;
+
+    for te in t {
+        let idx = s
+            .binary_search_by_key(&(&2 * te), |a| 2 * a + 1)
+            .err()
+            .unwrap();
+        if idx >= s.len() {
+            continue;
+        }
+        if te == s[idx] {
+            ans += 1;
+        }
+    }
+
+    println!("{}", ans);
+}
