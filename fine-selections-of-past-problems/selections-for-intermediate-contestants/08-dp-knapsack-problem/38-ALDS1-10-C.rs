3ab2e2467c63a5f0236892c49717651c90d37f33
Acasune <tttt4aaaa@gmail.com>
2021-02-07 16:48:16 +0900

    Solve: 38-ALDS1-10-C

---
+++ fine-selections-of-past-problems/selections-for-intermediate-contestants/08-dp-knapsack-problem/38-ALDS1-10-C.rs
@@ -0,0 +1,83 @@
+﻿use std::{
+    cmp::{max, min},
+    io::*,
+    str::FromStr,
+};
+
+#[allow(unused_macros)]
+macro_rules! scan {
+  ($e:expr; $t:ty) => {
+    $e.get::<$t>()
+  };
+  ($e:expr; $($t:ty), *) => {
+    ($($e.get::<$t>(),)*)
+  }
+}
+
+struct Scanner<R: BufRead> {
+    reader: R,
+    iter: std::vec::IntoIter<String>,
+}
+
+#[allow(dead_code)]
+impl<R: BufRead> Scanner<R> {
+    fn new(reader: R) -> Scanner<R> {
+        Scanner {
+            reader,
+            iter: vec![String::new()].into_iter(),
+        }
+    }
+    fn new_line(&mut self) {
+        let mut line = String::new();
+        self.reader.read_line(&mut line).ok();
+        self.iter = line
+            .trim()
+            .split_whitespace()
+            .map(|s| s.to_string())
+            .collect::<Vec<String>>()
+            .into_iter();
+    }
+    fn get<T: FromStr>(&mut self) -> T {
+        self.iter.next().unwrap().parse().ok().unwrap()
+    }
+    fn get_as_vec<T: FromStr>(&mut self) -> Vec<T> {
+        self.iter.clone().map(|v| v.parse().ok().unwrap()).collect()
+    }
+    fn get_line(&mut self) -> String {
+        let mut line = String::new();
+        self.reader.read_line(&mut line).ok();
+        line.trim().to_string()
+    }
+}
+
+fn main() {
+    let cin = stdin();
+    let cin = cin.lock();
+    let mut sc = Scanner::new(cin);
+
+    sc.new_line();
+    let n = sc.get::<usize>();
+
+    for i in 0..n {
+        sc.new_line();
+        let a: Vec<char> = sc.get::<String>().chars().collect();
+        sc.new_line();
+        let b: Vec<char> = sc.get::<String>().chars().collect();
+
+        let a_l: usize = a.len();
+        let b_l: usize = b.len();
+
+        let mut dp = vec![vec![0; b_l + 1]; a_l + 1];
+
+        for i in 0..a_l {
+            for j in 0..b_l {
+                if a[i] == b[j] {
+                    dp[i + 1][j + 1] = max(max(dp[i][j] + 1, dp[i][j + 1]), dp[i + 1][j])
+                } else {
+                    dp[i + 1][j + 1] = max(dp[i][j + 1], dp[i + 1][j])
+                }
+            }
+        }
+        println!("{}", dp[a_l][b_l]);
+    }
+}
