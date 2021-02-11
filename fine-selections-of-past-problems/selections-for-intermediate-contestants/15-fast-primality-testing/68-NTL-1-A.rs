8cf8764ae45746191ce7b434ae5a914667c3bf8a
Acasune <tttt4aaaa@gmail.com>
2021-02-11 23:59:08 +0900

    Solve: 68-NTL-1-A

---
+++ fine-selections-of-past-problems/selections-for-intermediate-contestants/15-fast-primality-testing/68-NTL-1-A.rs
@@ -0,0 +1,85 @@
+﻿use std::{
+    cmp::{max, min},
+    collections::HashSet,
+    io::*,
+    str::FromStr,
+};
+
+#[allow(unused_macros)]
+macro_rules! scan {
+  ($e:expr; $t:ty) => {
+    $e.get::<$t>()
+  };
+  ($e:expr; $($t:ty), *) => {
+    ($($e.get::<$t>(),)*)
+  }
+}
+
+struct Scanner<R: BufRead> {
+    reader: R,
+    iter: std::vec::IntoIter<String>,
+}
+
+#[allow(dead_code)]
+impl<R: BufRead> Scanner<R> {
+    fn new(reader: R) -> Scanner<R> {
+        Scanner {
+            reader,
+            iter: vec![String::new()].into_iter(),
+        }
+    }
+    fn new_line(&mut self) {
+        let mut line = String::new();
+        self.reader.read_line(&mut line).ok();
+        self.iter = line
+            .trim()
+            .split_whitespace()
+            .map(|s| s.to_string())
+            .collect::<Vec<String>>()
+            .into_iter();
+    }
+    fn get<T: FromStr>(&mut self) -> T {
+        self.iter.next().unwrap().parse().ok().unwrap()
+    }
+    fn get_as_vec<T: FromStr>(&mut self) -> Vec<T> {
+        self.iter.clone().map(|v| v.parse().ok().unwrap()).collect()
+    }
+    fn get_line(&mut self) -> String {
+        let mut line = String::new();
+        self.reader.read_line(&mut line).ok();
+        line.trim().to_string()
+    }
+}
+
+fn main() {
+    let cin = stdin();
+    let cin = cin.lock();
+    let mut sc = Scanner::new(cin);
+
+    sc.new_line();
+    let mut n = sc.get::<i64>();
+    let nn = n;
+    print!("{}:", n);
+    let mut v = vec![];
+
+    for i in 2.. {
+        if i * i > n {
+            if n > 2 {
+                v.push(n);
+            }
+            break;
+        }
+        if n % i == 0 {
+            while n % i == 0 {
+                v.push(i);
+                n /= i;
+            }
+        }
+    }
+
+    for e in v {
+        print!(" {}", e);
+    }
+
+    println!("");
+}
