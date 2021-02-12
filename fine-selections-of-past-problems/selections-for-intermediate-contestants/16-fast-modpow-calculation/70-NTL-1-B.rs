f52d0d80ecae0d0fde0965f6ae354630f72c2b5a
Acasune <tttt4aaaa@gmail.com>
2021-02-12 10:05:43 +0900

    Solve: 70-NTL-1-B

---
+++ fine-selections-of-past-problems/selections-for-intermediate-contestants/16-fast-modpow-calculation/70-NTL-1-B.rs
@@ -0,0 +1,79 @@
+﻿use std::{
+    cmp::{max, min},
+    collections::HashSet,
+    io::*,
+    str::FromStr,
+};
+
+#[allow(unused_macros)]
+macro_rules! scan {
+  ($e:expr; $t:ty) => {
+    $e.get::<$t>()
+  };
+  ($e:expr; $($t:ty), *) => {
+    ($($e.get::<$t>(),)*)
+  }
+}
+
+struct Scanner<R: BufRead> {
+    reader: R,
+    iter: std::vec::IntoIter<String>,
+}
+
+#[allow(dead_code)]
+impl<R: BufRead> Scanner<R> {
+    fn new(reader: R) -> Scanner<R> {
+        Scanner {
+            reader,
+            iter: vec![String::new()].into_iter(),
+        }
+    }
+    fn new_line(&mut self) {
+        let mut line = String::new();
+        self.reader.read_line(&mut line).ok();
+        self.iter = line
+            .trim()
+            .split_whitespace()
+            .map(|s| s.to_string())
+            .collect::<Vec<String>>()
+            .into_iter();
+    }
+    fn get<T: FromStr>(&mut self) -> T {
+        self.iter.next().unwrap().parse().ok().unwrap()
+    }
+    fn get_as_vec<T: FromStr>(&mut self) -> Vec<T> {
+        self.iter.clone().map(|v| v.parse().ok().unwrap()).collect()
+    }
+    fn get_line(&mut self) -> String {
+        let mut line = String::new();
+        self.reader.read_line(&mut line).ok();
+        line.trim().to_string()
+    }
+}
+
+fn main() {
+    let cin = stdin();
+    let cin = cin.lock();
+    let mut sc = Scanner::new(cin);
+
+    sc.new_line();
+    let m = sc.get::<u64>();
+    let n = sc.get::<u64>();
+    let md = 1_000_000_007;
+
+    println!("{}", mod_pow(m, n, md));
+}
+
+//return a^n%mod
+fn mod_pow(mut m: u64, mut n: u64, modulo: u64) -> u64 {
+    let mut ret: u64 = 1;
+    while n > 0 {
+        if n % 2 == 1 {
+            ret = ret * m % modulo;
+        }
+        m = m * m % modulo;
+        n = n / 2;
+    }
+
+    return ret;
+}
