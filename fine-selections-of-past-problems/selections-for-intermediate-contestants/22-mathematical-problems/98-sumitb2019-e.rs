5d1a8658cb06d1fac38f6fe25c9943379096c671
Acasune <tttt4aaaa@gmail.com>
2021-02-14 00:50:07 +0900

    Solve: 98-sumitb2019-e

---
+++ fine-selections-of-past-problems/selections-for-intermediate-contestants/22-mathematical-problems/98-sumitb2019-e.rs
@@ -0,0 +1,76 @@
+﻿use std::{
+    cmp::{max, min},
+    collections::HashSet,
+    io::*,
+    str::FromStr,
+};
+
+#[allow(unused_macros)]
+macro_rules! scan {
+  ($e:expr; $t:ty) => {
+    $e.get::<$t>()
+  };
+  ($e:expr; $($t:ty), *) => {
+    ($($e.get::<$t>(),)*)
+  }
+}
+
+struct Scanner<R: BufRead> {
+    reader: R,
+    iter: std::vec::IntoIter<String>,
+}
+
+#[allow(dead_code)]
+impl<R: BufRead> Scanner<R> {
+    fn new(reader: R) -> Scanner<R> {
+        Scanner {
+            reader,
+            iter: vec![String::new()].into_iter(),
+        }
+    }
+    fn new_line(&mut self) {
+        let mut line = String::new();
+        self.reader.read_line(&mut line).ok();
+        self.iter = line
+            .trim()
+            .split_whitespace()
+            .map(|s| s.to_string())
+            .collect::<Vec<String>>()
+            .into_iter();
+    }
+    fn get<T: FromStr>(&mut self) -> T {
+        self.iter.next().unwrap().parse().ok().unwrap()
+    }
+    fn get_as_vec<T: FromStr>(&mut self) -> Vec<T> {
+        self.iter.clone().map(|v| v.parse().ok().unwrap()).collect()
+    }
+    fn get_line(&mut self) -> String {
+        let mut line = String::new();
+        self.reader.read_line(&mut line).ok();
+        line.trim().to_string()
+    }
+}
+
+fn main() {
+    let cin = stdin();
+    let cin = cin.lock();
+    let mut sc = Scanner::new(cin);
+
+    sc.new_line();
+    let n = sc.get::<usize>();
+
+    sc.new_line();
+    let a = sc.get_as_vec::<usize>();
+    let mut ans = 1;
+    let md = 1_000_000_007;
+    let mut records = vec![0; n + 1];
+    records[0] = 3;
+
+    for i in 0..n {
+        let t = a[i];
+        ans *= (records[t] - records[t + 1]) as u128;
+        ans %= md;
+        records[t + 1] += 1;
+    }
+    println!("{}", ans);
+}
