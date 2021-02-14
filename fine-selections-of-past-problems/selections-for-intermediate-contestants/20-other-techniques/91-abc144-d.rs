cac178b1588db8b86aece40b7a5761acc8975a21
Acasune <tttt4aaaa@gmail.com>
2021-02-14 22:27:04 +0900

    Solve: 91-abc144-d

---
+++ fine-selections-of-past-problems/selections-for-intermediate-contestants/20-other-techniques/91-abc144-d.rs
@@ -0,0 +1,71 @@
+﻿use std::{io::*, str::FromStr};
+
+#[allow(unused_macros)]
+macro_rules! scan {
+  ($e:expr; $t:ty) => {
+    $e.get::<$t>()
+  };
+  ($e:expr; $($t:ty), *) => {
+    ($($e.get::<$t>(),)*)
+  }
+}
+
+struct Scanner<R: BufRead> {
+    reader: R,
+    iter: std::vec::IntoIter<String>,
+}
+
+#[allow(dead_code)]
+impl<R: BufRead> Scanner<R> {
+    fn new(reader: R) -> Scanner<R> {
+        Scanner {
+            reader,
+            iter: vec![String::new()].into_iter(),
+        }
+    }
+    fn new_line(&mut self) {
+        let mut line = String::new();
+        self.reader.read_line(&mut line).ok();
+        self.iter = line
+            .trim()
+            .split_whitespace()
+            .map(|s| s.to_string())
+            .collect::<Vec<String>>()
+            .into_iter();
+    }
+    fn get<T: FromStr>(&mut self) -> T {
+        self.iter.next().unwrap().parse().ok().unwrap()
+    }
+    fn get_as_vec<T: FromStr>(&mut self) -> Vec<T> {
+        self.iter.clone().map(|v| v.parse().ok().unwrap()).collect()
+    }
+    fn get_line(&mut self) -> String {
+        let mut line = String::new();
+        self.reader.read_line(&mut line).ok();
+        line.trim().to_string()
+    }
+}
+
+fn main() {
+    let cin = stdin();
+    let cin = cin.lock();
+    let mut sc = Scanner::new(cin);
+
+    sc.new_line();
+    let mut a = sc.get::<f64>();
+    let mut b = sc.get::<f64>();
+    let mut x = sc.get::<f64>();
+
+    let pi = std::f64::consts::PI;
+
+    if x <= (a * a * b) / 2. {
+        let mut rad = f64::atan(((2. * x) / (a * b)) / b);
+        let deg = rad * 180. / pi;
+        println!("{:.10}", 90. - deg)
+    } else {
+        let c = 2. * ((a * a * b - x) / (a * a));
+        let mut rad = f64::atan(a / c);
+        let deg = rad * 180. / pi;
+        println!("{}", 90. - deg)
+    }
+}
