88d2bc88fe73307834b3be7aa26809fc68594901
Acasune <tttt4aaaa@gmail.com>
2021-02-14 16:32:48 +0900

    Solve: 89-joi2013ho1

---
+++ fine-selections-of-past-problems/selections-for-intermediate-contestants/20-other-techniques/89-joi2013ho1.rs
@@ -0,0 +1,82 @@
+﻿use std::{
+    cmp::{max, min},
+    collections::HashSet,
+    io::*,
+    str::FromStr,
+};
+
+#[allow(unused_macros)]
+macro_rules! scan {
+  ($e:expr; $t:ty) => {
+    $e.get::<$t>()
+  };
+  ($e:expr; $($t:ty), *) => {
+    ($($e.get::<$t>(),)*)
+  }
+}
+
+struct Scanner<R: BufRead> {
+    reader: R,
+    iter: std::vec::IntoIter<String>,
+}
+
+#[allow(dead_code)]
+impl<R: BufRead> Scanner<R> {
+    fn new(reader: R) -> Scanner<R> {
+        Scanner {
+            reader,
+            iter: vec![String::new()].into_iter(),
+        }
+    }
+    fn new_line(&mut self) {
+        let mut line = String::new();
+        self.reader.read_line(&mut line).ok();
+        self.iter = line
+            .trim()
+            .split_whitespace()
+            .map(|s| s.to_string())
+            .collect::<Vec<String>>()
+            .into_iter();
+    }
+    fn get<T: FromStr>(&mut self) -> T {
+        self.iter.next().unwrap().parse().ok().unwrap()
+    }
+    fn get_as_vec<T: FromStr>(&mut self) -> Vec<T> {
+        self.iter.clone().map(|v| v.parse().ok().unwrap()).collect()
+    }
+    fn get_line(&mut self) -> String {
+        let mut line = String::new();
+        self.reader.read_line(&mut line).ok();
+        line.trim().to_string()
+    }
+}
+
+fn main() {
+    let cin = stdin();
+    let cin = cin.lock();
+    let mut sc = Scanner::new(cin);
+
+    sc.new_line();
+    let mut n = sc.get::<usize>();
+    sc.new_line();
+    let mut a = sc.get_as_vec::<usize>();
+
+    let mut memo = vec![0; n + 3];
+
+    let mut right = 0;
+    memo[0] = 1;
+    for i in 1..n {
+        if a[i - 1] != a[i] {
+            memo[right] += 1;
+        } else {
+            right += 1;
+            memo[right] = 1;
+        }
+    }
+    let mut ans = 0;
+    for i in 0..n {
+        ans = max(ans, memo[i] + memo[i + 1] + memo[i + 2]);
+    }
+
+    println!("{}", ans);
+}
