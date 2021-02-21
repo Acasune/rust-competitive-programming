4daa71db2949ba3b99ca12934ba966b2c275d25b
Acasune <tttt4aaaa@gmail.com>
2021-02-21 11:59:00 +0900

    Contest: abc-192

---
+++ AtCoder/abc/abc192/c.rs
@@ -0,0 +1,81 @@
+use std::iter::FromIterator;
+use std::{
+    cmp::{max, min},
+    collections::HashSet,
+    io::*,
+    str::FromStr,
+};
+
+#[allow(unused_macros)]
+macro_rules! scan {
+  ($e:expr; $t:ty) => {
+    $e.get::<$t>()
+  };
+  ($e:expr; $($t:ty), *) => {
+    ($($e.get::<$t>(),)*)
+  }
+}
+
+struct Scanner<R: BufRead> {
+    reader: R,
+    iter: std::vec::IntoIter<String>,
+}
+
+#[allow(dead_code)]
+impl<R: BufRead> Scanner<R> {
+    fn new(reader: R) -> Scanner<R> {
+        Scanner {
+            reader,
+            iter: vec![String::new()].into_iter(),
+        }
+    }
+    fn new_line(&mut self) {
+        let mut line = String::new();
+        self.reader.read_line(&mut line).ok();
+        self.iter = line
+            .trim()
+            .split_whitespace()
+            .map(|s| s.to_string())
+            .collect::<Vec<String>>()
+            .into_iter();
+    }
+    fn get<T: FromStr>(&mut self) -> T {
+        self.iter.next().unwrap().parse().ok().unwrap()
+    }
+    fn get_as_vec<T: FromStr>(&mut self) -> Vec<T> {
+        self.iter.clone().map(|v| v.parse().ok().unwrap()).collect()
+    }
+    fn get_line(&mut self) -> String {
+        let mut line = String::new();
+        self.reader.read_line(&mut line).ok();
+        line.trim().to_string()
+    }
+}
+
+fn main() {
+    let cin = stdin();
+    let cin = cin.lock();
+    let mut sc = Scanner::new(cin);
+
+    sc.new_line();
+    let mut n = sc.get::<i64>();
+    let mut k = sc.get::<i64>();
+
+    let mut ans = n;
+    for i in 0..k {
+        ans = g1(ans);
+    }
+
+    println!("{}", ans);
+}
+
+fn g1(a: i64) -> i64 {
+    let mut s: Vec<char> = a.to_string().chars().collect();
+    let mut s1: Vec<char> = a.to_string().chars().collect();
+    s.sort();
+    let b = s.iter().collect::<String>().parse::<i64>().unwrap();
+    // s1.sort();
+    s.reverse();
+    let c = s.into_iter().collect::<String>().parse::<i64>().unwrap();
+    return c - b;
+}
