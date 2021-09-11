5bca7503ccc6609cc31c863899201f556cdfd807
Acasune <tttt4aaaa@gmail.com>
2021-09-11 10:23:35 +0900

    Solve: ALDS1_1_A

---
+++ AOJ/ALDS1/ALDS1_1_A.rs
@@ -0,0 +1,81 @@
+﻿use std::cmp::{max, min};
+use std::{io::*, str::FromStr};
+
+#[allow(unused_macros)]
+macro_rules! scan {
+  ($e:expr; $t:ty) => {
+    $e.get::<$t>()
+  };
+  ($e:expr; $($t:ty), *) => {
+    ($($e.get::<$t>(),)*)
+  }
+}
+
+struct Scanner<R: BufRead> {
+    reader: R,
+    iter: std::vec::IntoIter<String>,
+}
+
+#[allow(dead_code)]
+impl<R: BufRead> Scanner<R> {
+    fn new(reader: R) -> Scanner<R> {
+        Scanner {
+            reader,
+            iter: vec![String::new()].into_iter(),
+        }
+    }
+    fn new_line(&mut self) {
+        let mut line = String::new();
+        self.reader.read_line(&mut line).ok();
+        self.iter = line
+            .trim()
+            .split_whitespace()
+            .map(|s| s.to_string())
+            .collect::<Vec<String>>()
+            .into_iter();
+    }
+    fn get<T: FromStr>(&mut self) -> T {
+        self.iter.next().unwrap().parse().ok().unwrap()
+    }
+    fn get_as_vec<T: FromStr>(&mut self) -> Vec<T> {
+        self.iter.clone().map(|v| v.parse().ok().unwrap()).collect()
+    }
+    fn get_line(&mut self) -> String {
+        let mut line = String::new();
+        self.reader.read_line(&mut line).ok();
+        line.trim().to_string()
+    }
+}
+
+fn main() {
+    let cin = stdin();
+    let cin = cin.lock();
+    let mut sc = Scanner::new(cin);
+
+    sc.new_line();
+    let n: i64 = sc.get();
+    sc.new_line();
+    let mut v: Vec<i64> = sc.get_as_vec();
+    insertion_sort(&mut v, n);
+}
+
+fn insertion_sort(v: &mut Vec<i64>, n: i64) {
+    print_vec(&v);
+    for i in 1..n {
+        let mut j = i - 1;
+        let mut a = v[i as usize];
+        while j >= 0 && v[j as usize] > a {
+            v[(j + 1) as usize] = v[j as usize];
+            j -= 1;
+        }
+        v[(j + 1) as usize] = a;
+        print_vec(&v);
+    }
+}
+
+fn print_vec(v: &Vec<i64>) {
+    for i in 0..v.len() - 1 {
+        print!("{} ", v[i]);
+    }
+    println!("{}", v[v.len() - 1]);
+}
