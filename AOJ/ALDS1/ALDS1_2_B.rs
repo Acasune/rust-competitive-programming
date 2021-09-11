b7a48e3f0a04187ed54c9151ac48bc188635e86b
Acasune <tttt4aaaa@gmail.com>
2021-09-11 13:38:18 +0900

    Solve: ALDS1_2_B

---
+++ AOJ/ALDS1/ALDS1_2_B.rs
@@ -0,0 +1,94 @@
+﻿#[allow(unused_imports)]
+use std::cmp::{max, min};
+use std::{io::*, str::FromStr};
+
+#[allow(unused_macros)]
+macro_rules! scan {
+  ($e:expr; $t:ty) => {
+    $e.get::<$t>()
+  };
+  ($e:expr; $($t:ty), *) => {
+    ($($e.get::<$t>(),)*)
+  }
+}
+
+struct Scanner<R: BufRead> {
+    reader: R,
+    iter: std::vec::IntoIter<String>,
+}
+
+#[allow(dead_code)]
+impl<R: BufRead> Scanner<R> {
+    fn new(reader: R) -> Scanner<R> {
+        Scanner {
+            reader,
+            iter: vec![String::new()].into_iter(),
+        }
+    }
+    fn new_line(&mut self) {
+        let mut line = String::new();
+        self.reader.read_line(&mut line).ok();
+        self.iter = line
+            .trim()
+            .split_whitespace()
+            .map(|s| s.to_string())
+            .collect::<Vec<String>>()
+            .into_iter();
+    }
+    fn get<T: FromStr>(&mut self) -> T {
+        self.iter.next().unwrap().parse().ok().unwrap()
+    }
+    fn get_as_vec<T: FromStr>(&mut self) -> Vec<T> {
+        self.iter.clone().map(|v| v.parse().ok().unwrap()).collect()
+    }
+    fn get_line(&mut self) -> String {
+        let mut line = String::new();
+        self.reader.read_line(&mut line).ok();
+        line.trim().to_string()
+    }
+}
+
+#[allow(unused_variables)]
+fn main() {
+    let cin = stdin();
+    let cin = cin.lock();
+    let mut sc = Scanner::new(cin);
+
+    sc.new_line();
+    let n: usize = sc.get();
+    sc.new_line();
+    let mut v: Vec<i64> = sc.get_as_vec();
+
+    selection_sort(&mut v);
+}
+
+fn selection_sort(v: &mut Vec<i64>) {
+    let mut cnt = 0;
+    let len: usize = v.len();
+
+    for i in 0..len {
+        let mut min = v[i];
+        let mut idx = i;
+        for j in i + 1..len {
+            if v[j] < min {
+                min = v[j];
+                idx = j;
+            }
+        }
+        if v[i] > v[idx] {
+            v[idx] = v[i];
+            v[i] = min;
+            cnt += 1;
+        }
+    }
+
+    print_vec(&v);
+    println!("{}", cnt);
+}
+
+fn print_vec(v: &Vec<i64>) {
+    for i in 0..v.len() - 1 {
+        print!("{} ", v[i]);
+    }
+    println!("{}", v[v.len() - 1]);
+}
