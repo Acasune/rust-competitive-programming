f2ad81d21dbea71f00755e0492f9ded61042be70
Acasune <tttt4aaaa@gmail.com>
2021-10-03 14:13:43 +0900

    Solve: ALDS1_9_A

---
+++ AOJ/ALDS1/ALDS1_9_A.rs
@@ -0,0 +1,81 @@
+﻿#[allow(unused_imports)]
+use std::cmp::{max, min};
+use std::{
+    cell::RefCell,
+    collections::HashMap,
+    collections::VecDeque,
+    fmt::Display,
+    rc::{Rc, Weak},
+};
+use std::{io::*, str::FromStr};
+
+#[allow(unused_macros)]
+macro_rules! scan {
+  ($e:expr; $t:ty) => {
+    $e.get::<$t>()
+  };
+  ($e:expr; $($t:ty), *) => {
+    ($($e.get::<$t>(),)*)
+  }
+}
+
+struct Scanner<R: BufRead> {
+    reader: R,
+    iter: std::vec::IntoIter<String>,
+}
+
+#[allow(dead_code)]
+impl<R: BufRead> Scanner<R> {
+    fn new(reader: R) -> Scanner<R> {
+        Scanner {
+            reader,
+            iter: vec![String::new()].into_iter(),
+        }
+    }
+    fn new_line(&mut self) {
+        let mut line = String::new();
+        self.reader.read_line(&mut line).ok();
+        self.iter = line
+            .trim()
+            .split_whitespace()
+            .map(|s| s.to_string())
+            .collect::<Vec<String>>()
+            .into_iter();
+    }
+    fn get<T: FromStr>(&mut self) -> T {
+        self.iter.next().unwrap().parse().ok().unwrap()
+    }
+    fn get_as_vec<T: FromStr>(&mut self) -> Vec<T> {
+        self.iter.clone().map(|v| v.parse().ok().unwrap()).collect()
+    }
+    fn get_line(&mut self) -> String {
+        let mut line = String::new();
+        self.reader.read_line(&mut line).ok();
+        line.trim().to_string()
+    }
+}
+
+#[allow(unused_variables)]
+fn main() {
+    let cin = stdin();
+    let cin = cin.lock();
+    let mut sc = Scanner::new(cin);
+
+    sc.new_line();
+    let n = sc.get::<usize>();
+    sc.new_line();
+    let vec = sc.get_as_vec::<i64>();
+    for i in 0..n {
+        print!("node {}: key = {}, ", i + 1, vec[i]);
+        if i > 0 {
+            print!("parent key = {}, ", vec[(i - 1) / 2]);
+        }
+        if i * 2 + 1 < n {
+            print!("left key = {}, ", vec[i * 2 + 1]);
+        }
+        if i * 2 + 2 < n {
+            print!("right key = {}, ", vec[i * 2 + 2]);
+        }
+        println!();
+    }
+}
