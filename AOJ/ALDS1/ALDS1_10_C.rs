a182141b93d2f753349b9669773106c667bf33f1
Acasune <tttt4aaaa@gmail.com>
2021-10-09 21:25:16 +0900

    Solve: ALDS1_10_C

---
+++ AOJ/ALDS1/ALDS1_10_C.rs
@@ -0,0 +1,85 @@
+﻿#[allow(unused_imports)]
+use std::cmp::{max, min};
+use std::{
+    cell::RefCell,
+    collections::HashMap,
+    collections::VecDeque,
+    fmt::Display,
+    rc::{Rc, Weak},
+};
+use std::{io::*, str::FromStr};
+
+#[allow(unused_macros)]
+macro_rules! scan {
+  ($e:expr; $t:ty) => {
+    $e.get::<$t>()
+  };
+  ($e:expr; $($t:ty), *) => {
+    ($($e.get::<$t>(),)*)
+  }
+}
+
+struct Scanner<R: BufRead> {
+    reader: R,
+    iter: std::vec::IntoIter<String>,
+}
+
+#[allow(dead_code)]
+impl<R: BufRead> Scanner<R> {
+    fn new(reader: R) -> Scanner<R> {
+        Scanner {
+            reader,
+            iter: vec![String::new()].into_iter(),
+        }
+    }
+    fn new_line(&mut self) {
+        let mut line = String::new();
+        self.reader.read_line(&mut line).ok();
+        self.iter = line
+            .trim()
+            .split_whitespace()
+            .map(|s| s.to_string())
+            .collect::<Vec<String>>()
+            .into_iter();
+    }
+    fn get<T: FromStr>(&mut self) -> T {
+        self.iter.next().unwrap().parse().ok().unwrap()
+    }
+    fn get_as_vec<T: FromStr>(&mut self) -> Vec<T> {
+        self.iter.clone().map(|v| v.parse().ok().unwrap()).collect()
+    }
+    fn get_line(&mut self) -> String {
+        let mut line = String::new();
+        self.reader.read_line(&mut line).ok();
+        line.trim().to_string()
+    }
+}
+
+#[allow(unused_variables)]
+fn main() {
+    let cin = stdin();
+    let cin = cin.lock();
+    let mut sc = Scanner::new(cin);
+
+    sc.new_line();
+    let n = sc.get::<usize>();
+
+    for _ in 0..n {
+        sc.new_line();
+        let a: Vec<char> = sc.get::<String>().chars().collect();
+        sc.new_line();
+        let b: Vec<char> = sc.get::<String>().chars().collect();
+        let mut memo = vec![vec![0; a.len() + 1]; b.len() + 1];
+
+        for j in 0..b.len() {
+            for i in 0..a.len() {
+                if a[i] == b[j] {
+                    memo[j + 1][i + 1] = memo[j][i] + 1;
+                } else {
+                    memo[j + 1][i + 1] = max(memo[j][i + 1], memo[j + 1][i]);
+                }
+            }
+        }
+        println!("{}", memo[b.len()][a.len()]);
+    }
+}
