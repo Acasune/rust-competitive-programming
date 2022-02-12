31ee88d448045764ac91d741bdd10e3904787ae4
Acasune <tttt4aaaa@gmail.com>
2022-02-12 14:06:30 +0900

    Solve:t90p 061

---
+++ typical-90problems-of-competitive-programming/src/061.rs
@@ -0,0 +1,72 @@
+use itertools::Itertools;
+use std::cmp::{max, min, Reverse};
+use std::collections::{BinaryHeap, HashMap, HashSet, VecDeque};
+use std::{i64, io::*, str::FromStr, usize};
+
+#[allow(unused_macros)]
+macro_rules! scan {
+  ($e:expr; $t:ty) => {
+    $e.get::<$t>()
+  };
+  ($e:expr; $($t:ty), *) => {
+    ($($e.get::<$t>(),)*)
+  }
+}
+
+struct Scanner<R: BufRead> {
+    reader: R,
+    iter: std::vec::IntoIter<String>,
+}
+
+#[allow(dead_code)]
+impl<R: BufRead> Scanner<R> {
+    fn new(reader: R) -> Scanner<R> {
+        Scanner {
+            reader,
+            iter: vec![String::new()].into_iter(),
+        }
+    }
+    fn new_line(&mut self) {
+        let mut line = String::new();
+        self.reader.read_line(&mut line).ok();
+        self.iter = line
+            .trim()
+            .split_whitespace()
+            .map(|s| s.to_string())
+            .collect::<Vec<String>>()
+            .into_iter();
+    }
+    fn get<T: FromStr>(&mut self) -> T {
+        self.iter.next().unwrap().parse().ok().unwrap()
+    }
+    fn get_as_vec<T: FromStr>(&mut self) -> Vec<T> {
+        self.iter.clone().map(|v| v.parse().ok().unwrap()).collect()
+    }
+    fn get_line(&mut self) -> String {
+        let mut line = String::new();
+        self.reader.read_line(&mut line).ok();
+        line.trim().to_string()
+    }
+}
+
+fn main() {
+    let cin = stdin();
+    let cin = cin.lock();
+    let mut sc = Scanner::new(cin);
+
+    sc.new_line();
+    let mut Q: usize = sc.get();
+    let mut deq = VecDeque::<usize>::new();
+    for q in 0..Q {
+        sc.new_line();
+        let t: usize = sc.get();
+        let x: usize = sc.get();
+        if t == 1 {
+            deq.push_front(x);
+        } else if t == 2 {
+            deq.push_back(x);
+        } else if t == 3 {
+            println!("{}", deq[x - 1]);
+        }
+    }
+}
