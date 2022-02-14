329f21e4af6ac318603f862c45c048c917f8d2ba
Acasune <tttt4aaaa@gmail.com>
2022-02-14 22:35:42 +0900

    Solve:t90p 078

---
+++ typical-90problems-of-competitive-programming/src/078.rs
@@ -0,0 +1,81 @@
+use itertools::Itertools;
+use std::cmp::{max, min, Reverse};
+use std::collections::{BinaryHeap, HashMap, HashSet, VecDeque};
+use std::{i64, io::*, str::FromStr, usize};
+
+#[allow(unused_macros)]
+macro_rules! scan {
+  ($e:expr; $t:ty) => {
+    $e.get::<$t>()
+  };
+  ($e:expr; $($t:ty), *) => {
+    ($($e.get::<$t>(),)*)
+  }
+}
+
+struct Scanner<R: BufRead> {
+    reader: R,
+    iter: std::vec::IntoIter<String>,
+}
+
+#[allow(dead_code)]
+impl<R: BufRead> Scanner<R> {
+    fn new(reader: R) -> Scanner<R> {
+        Scanner {
+            reader,
+            iter: vec![String::new()].into_iter(),
+        }
+    }
+    fn new_line(&mut self) {
+        let mut line = String::new();
+        self.reader.read_line(&mut line).ok();
+        self.iter = line
+            .trim()
+            .split_whitespace()
+            .map(|s| s.to_string())
+            .collect::<Vec<String>>()
+            .into_iter();
+    }
+    fn get<T: FromStr>(&mut self) -> T {
+        self.iter.next().unwrap().parse().ok().unwrap()
+    }
+    fn get_as_vec<T: FromStr>(&mut self) -> Vec<T> {
+        self.iter.clone().map(|v| v.parse().ok().unwrap()).collect()
+    }
+    fn get_line(&mut self) -> String {
+        let mut line = String::new();
+        self.reader.read_line(&mut line).ok();
+        line.trim().to_string()
+    }
+}
+
+fn main() {
+    let cin = stdin();
+    let cin = cin.lock();
+    let mut sc = Scanner::new(cin);
+
+    sc.new_line();
+    let N = sc.get::<usize>();
+    let M = sc.get::<usize>();
+    let mut edges = vec![Vec::<usize>::new(); N];
+    for m in 0..M {
+        sc.new_line();
+        let a = sc.get::<usize>() - 1;
+        let b = sc.get::<usize>() - 1;
+        edges[a].push(b);
+        edges[b].push(a);
+    }
+    let mut ret = 0;
+    for i in 0..N {
+        let mut cnt = 0;
+        for &j in &edges[i] {
+            if j < i {
+                cnt += 1;
+            }
+        }
+        if cnt == 1 {
+            ret += 1;
+        }
+    }
+    println!("{}", ret);
+}
