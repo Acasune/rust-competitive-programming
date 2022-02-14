cea1423bac07305eeaa497fb53ffe957379152ed
Acasune <tttt4aaaa@gmail.com>
2022-02-14 21:19:13 +0900

    Solve:t90p 075

---
+++ typical-90problems-of-competitive-programming/src/075.rs
@@ -0,0 +1,78 @@
+use itertools::Itertools;
+use std::cmp::{max, min, Reverse};
+use std::collections::{BinaryHeap, HashMap, HashSet, VecDeque};
+use std::{i64, io::*, str::FromStr, usize};
+
+#[allow(unused_macros)]
+macro_rules! scan {
+  ($e:expr; $t:ty) => {
+    $e.get::<$t>()
+  };
+  ($e:expr; $($t:ty), *) => {
+    ($($e.get::<$t>(),)*)
+  }
+}
+
+struct Scanner<R: BufRead> {
+    reader: R,
+    iter: std::vec::IntoIter<String>,
+}
+
+#[allow(dead_code)]
+impl<R: BufRead> Scanner<R> {
+    fn new(reader: R) -> Scanner<R> {
+        Scanner {
+            reader,
+            iter: vec![String::new()].into_iter(),
+        }
+    }
+    fn new_line(&mut self) {
+        let mut line = String::new();
+        self.reader.read_line(&mut line).ok();
+        self.iter = line
+            .trim()
+            .split_whitespace()
+            .map(|s| s.to_string())
+            .collect::<Vec<String>>()
+            .into_iter();
+    }
+    fn get<T: FromStr>(&mut self) -> T {
+        self.iter.next().unwrap().parse().ok().unwrap()
+    }
+    fn get_as_vec<T: FromStr>(&mut self) -> Vec<T> {
+        self.iter.clone().map(|v| v.parse().ok().unwrap()).collect()
+    }
+    fn get_line(&mut self) -> String {
+        let mut line = String::new();
+        self.reader.read_line(&mut line).ok();
+        line.trim().to_string()
+    }
+}
+
+fn main() {
+    let cin = stdin();
+    let cin = cin.lock();
+    let mut sc = Scanner::new(cin);
+
+    sc.new_line();
+    let N = sc.get::<usize>();
+    let mut n_prime = 0;
+    let mut base = 2;
+    let mut n = N;
+    while base * base <= n {
+        while n % base == 0 {
+            n_prime += 1;
+            n /= base;
+        }
+        base += 1;
+    }
+    if n != 1 {
+        n_prime += 1;
+    }
+    let mut memo = vec![0i64; n_prime + 10];
+    memo[2] = 1;
+    for i in 3..=n_prime {
+        memo[i] = max(memo[i / 2], memo[(i + 1) / 2]) + 1;
+    }
+    println!("{}", memo[n_prime]);
+}
