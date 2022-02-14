9ac415daef4fa2abdc35572331e646441ab60709
Acasune <tttt4aaaa@gmail.com>
2022-02-14 11:08:46 +0900

    Solve:t90p 070

---
+++ typical-90problems-of-competitive-programming/src/070.rs
@@ -0,0 +1,76 @@
+use itertools::Itertools;
+use std::cmp::{max, min, Reverse};
+use std::collections::{BinaryHeap, HashMap, HashSet, VecDeque};
+use std::{i64, io::*, str::FromStr, usize};
+
+#[allow(unused_macros)]
+macro_rules! scan {
+  ($e:expr; $t:ty) => {
+    $e.get::<$t>()
+  };
+  ($e:expr; $($t:ty), *) => {
+    ($($e.get::<$t>(),)*)
+  }
+}
+
+struct Scanner<R: BufRead> {
+    reader: R,
+    iter: std::vec::IntoIter<String>,
+}
+
+#[allow(dead_code)]
+impl<R: BufRead> Scanner<R> {
+    fn new(reader: R) -> Scanner<R> {
+        Scanner {
+            reader,
+            iter: vec![String::new()].into_iter(),
+        }
+    }
+    fn new_line(&mut self) {
+        let mut line = String::new();
+        self.reader.read_line(&mut line).ok();
+        self.iter = line
+            .trim()
+            .split_whitespace()
+            .map(|s| s.to_string())
+            .collect::<Vec<String>>()
+            .into_iter();
+    }
+    fn get<T: FromStr>(&mut self) -> T {
+        self.iter.next().unwrap().parse().ok().unwrap()
+    }
+    fn get_as_vec<T: FromStr>(&mut self) -> Vec<T> {
+        self.iter.clone().map(|v| v.parse().ok().unwrap()).collect()
+    }
+    fn get_line(&mut self) -> String {
+        let mut line = String::new();
+        self.reader.read_line(&mut line).ok();
+        line.trim().to_string()
+    }
+}
+
+fn main() {
+    let cin = stdin();
+    let cin = cin.lock();
+    let mut sc = Scanner::new(cin);
+
+    sc.new_line();
+    let N = sc.get::<usize>();
+    let mut X = vec![0; N];
+    let mut Y = vec![0; N];
+    for i in 0..N {
+        sc.new_line();
+        X[i] = sc.get::<i64>();
+        Y[i] = sc.get::<i64>();
+    }
+    X.sort();
+    Y.sort();
+    let mid_x = X[N / 2];
+    let mid_y = Y[N / 2];
+    let mut ret = 0;
+    for i in 0..N {
+        ret += (X[i] - mid_x).abs();
+        ret += (Y[i] - mid_y).abs();
+    }
+    println!("{}", ret);
+}
