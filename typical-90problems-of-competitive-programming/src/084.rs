001178c94f650bc50baf1c32d50de6af06de0447
Acasune <tttt4aaaa@gmail.com>
2022-02-16 21:05:47 +0900

    Solve:t90p 084

---
+++ typical-90problems-of-competitive-programming/src/084.rs
@@ -0,0 +1,77 @@
+use itertools::Itertools;
+use std::cmp::{max, min, Reverse};
+use std::collections::{BinaryHeap, HashMap, HashSet, VecDeque};
+use std::{i64, io::*, str::FromStr, usize};
+
+#[allow(unused_macros)]
+macro_rules! scan {
+  ($e:expr; $t:ty) => {
+    $e.get::<$t>()
+  };
+  ($e:expr; $($t:ty), *) => {
+    ($($e.get::<$t>(),)*)
+  }
+}
+
+struct Scanner<R: BufRead> {
+    reader: R,
+    iter: std::vec::IntoIter<String>,
+}
+
+#[allow(dead_code)]
+impl<R: BufRead> Scanner<R> {
+    fn new(reader: R) -> Scanner<R> {
+        Scanner {
+            reader,
+            iter: vec![String::new()].into_iter(),
+        }
+    }
+    fn new_line(&mut self) {
+        let mut line = String::new();
+        self.reader.read_line(&mut line).ok();
+        self.iter = line
+            .trim()
+            .split_whitespace()
+            .map(|s| s.to_string())
+            .collect::<Vec<String>>()
+            .into_iter();
+    }
+    fn get<T: FromStr>(&mut self) -> T {
+        self.iter.next().unwrap().parse().ok().unwrap()
+    }
+    fn get_as_vec<T: FromStr>(&mut self) -> Vec<T> {
+        self.iter.clone().map(|v| v.parse().ok().unwrap()).collect()
+    }
+    fn get_line(&mut self) -> String {
+        let mut line = String::new();
+        self.reader.read_line(&mut line).ok();
+        line.trim().to_string()
+    }
+}
+
+fn main() {
+    let cin = stdin();
+    let cin = cin.lock();
+    let mut sc = Scanner::new(cin);
+
+    sc.new_line();
+    let N = sc.get::<usize>();
+    sc.new_line();
+    let S = sc.get::<String>().chars().collect::<Vec<char>>();
+    let mut ans = sum_fn(N);
+    let mut c = S[0];
+    let mut cnt = 1;
+    for i in 1..N {
+        if c != S[i] {
+            ans -= sum_fn(cnt);
+            c = S[i];
+            cnt = 1;
+        } else {
+            cnt += 1;
+        }
+    }
+    println!("{}", ans - sum_fn(cnt));
+}
+fn sum_fn(a: usize) -> usize {
+    return a * (a + 1) / 2;
+}
