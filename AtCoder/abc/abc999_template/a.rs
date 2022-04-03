c7df443ce828ae00d9e7c7f697cd2d68e81c869c
Acasune <tttt4aaaa@gmail.com>
2022-04-03 11:26:13 +0900

    Refactor:templates

--- AtCoder/abc/abc999_template/a.rs
+++ AtCoder/abc/abc999_template/a.rs
@@ -6,12 +6,16 @@
 
 use proconio::marker::*;
 use proconio::*;
+use std::cmp::Reverse;
 use std::collections::{BinaryHeap, HashMap, HashSet};
 use std::{f64, i64, usize};
 
 const inf: i64 = i64::MAX / 10;
-const md: i64 = 1_000_000_007;
+const md1000000007: i64 = 1_000_000_007;
+const md998244353: i64 = 998244353;
 const eps: f64 = 1e-10;
+const dy: [i64; 4] = [1, -1, 1, -1];
+const dx: [i64; 4] = [1, 1, -1, -1];
 
 fn main() {
     input! {
