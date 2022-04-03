bac04d6851ff67d0c98dad2872b4e2b49f420348
Acasune <tttt4aaaa@gmail.com>
2022-04-03 22:39:31 +0900

    Refactor:246 e

--- AtCoder/abc/abc246/e.rs
+++ AtCoder/abc/abc246/e.rs
@@ -25,36 +25,11 @@ fn main() {
     let mut que = VecDeque::new();
     let dy = [1, -1, 1, -1];
     let dx = [1, 1, -1, -1];
-    let mut visited = vec![vec![vec![inf; 4]; N]; N];
-    let mut visited2 = vec![vec![vec![false; 4]; N]; N];
-    for i in 0..4 {
-        let dy = dy[i];
-        let dx = dx[i];
-        if !(0 <= ay + dy && ay + dy < N as i64) {
-            continue;
-        }
-        if !(0 <= ax + dx && ax + dx < N as i64) {
-            continue;
-        }
-        let ny = (ay + dy) as usize;
-        let nx = (ax + dx) as usize;
-        if board[ny][nx] == '#' {
-            continue;
-        }
-        que.push_back((ay, ax, i));
-        visited[ay as usize][ax as usize][i] = 1;
-    }
-    while let Some((y, x, pdir)) = que.pop_front() {
-        if y as usize == by && x as usize == bx {
-            println!("{}", visited[y as usize][x as usize][pdir as usize]);
-            return;
-        }
-        if visited2[y as usize][x as usize][pdir as usize] {
-            continue;
-        }
+    let mut visited = vec![vec![vec![inf; 5]; N]; N];
+    que.push_back((ay, ax, 4));
+    visited[ay as usize][ax as usize][4] = 0;
 
-        visited2[y as usize][x as usize][pdir as usize] = true;
-        let mut pval = visited[y as usize][x as usize][pdir as usize];
+    while let Some((y, x, pdir)) = que.pop_front() {
         for i in 0..4 {
             let ndy = dy[i];
             let ndx = dx[i];
@@ -71,22 +46,26 @@ fn main() {
             if board[ny][nx] == '#' {
                 continue;
             }
-            let ndir = i;
-            let mut val = pval;
-
-            if ndir != pdir {
-                val += 1;
-            }
-            if visited[ny][nx][ndir as usize] > val {
-                visited[ny][nx][ndir as usize] = val;
-                if ndir == pdir {
-                    que.push_front((ny as i64, nx as i64, ndir));
-                } else {
-                    que.push_back((ny as i64, nx as i64, ndir));
+            if pdir == i {
+                if visited[ny][nx][i] > visited[y as usize][x as usize][pdir as usize] {
+                    visited[ny][nx][i] = visited[y as usize][x as usize][pdir as usize];
+                    que.push_front((ny as i64, nx as i64, i));
+                }
+            } else {
+                if visited[ny][nx][i] > visited[y as usize][x as usize][pdir as usize] + 1 {
+                    visited[ny][nx][i] = visited[y as usize][x as usize][pdir as usize] + 1;
+                    que.push_back((ny as i64, nx as i64, i));
                 }
             }
         }
     }
-
-    println!("{}", -1);
+    let mut ans = inf;
+    for i in 0..4 {
+        ans = ans.min(visited[by][bx][i]);
+    }
+    if ans == inf {
+        println!("{}", -1);
+    } else {
+        println!("{}", ans);
+    }
 }
