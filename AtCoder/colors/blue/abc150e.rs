b94e06bf01a4fb2fd64a5100c94f36281fee0505
Acasune <tttt4aaaa@gmail.com>
2022-07-11 21:47:53 +0900

    Solve:abc150-e

---
+++ AtCoder/colors/blue/abc150e.rs
@@ -0,0 +1,46 @@
+#![allow(unused_imports)]
+#![allow(non_snake_case)]
+#![allow(dead_code)]
+#![allow(non_upper_case_globals)]
+#![allow(unused_variables)]
+
+use proconio::marker::*;
+use proconio::*;
+use std::cmp::Reverse;
+use std::collections::{BinaryHeap, HashMap, HashSet};
+use std::{char, f32, f64, i32, i64, usize};
+
+const inf_i: i64 = i64::MAX / 10;
+const inf_u: usize = usize::MAX / 10;
+const md1000000007: i64 = 1_000_000_007;
+const md998244353: i64 = 998244353;
+const eps: f64 = 1e-10;
+const d8yx: [(i64, i64); 8] = [
+    (1, 0),
+    (1, 1),
+    (0, 1),
+    (-1, 1),
+    (-1, 0),
+    (-1, -1),
+    (0, -1),
+    (1, -1),
+];
+const d4yx: [(i64, i64); 4] = [(1, 0), (0, 1), (-1, 0), (0, -1)];
+
+fn main() {
+    input! {
+        N:usize,
+        mut C:[i64;N]
+    }
+    let mut ans = 0;
+    let md = md1000000007;
+    C.sort();
+    C.reverse();
+    for i in 0..N {
+        ans = (ans + (i as i64 + 2) * C[i]) % md;
+    }
+    for i in 0..N - 1 {
+        ans = (ans * 4) % md;
+    }
+    println!("{}", ans);
+}
