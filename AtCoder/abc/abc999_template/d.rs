50f6556dc4d3f9966bfa27ba554a16d0c3fc5e3e
Acasune <tttt4aaaa@gmail.com>
2022-09-14 00:45:02 +0900

    Modify: max vals in template

--- AtCoder/abc/abc999_template/d.rs
+++ AtCoder/abc/abc999_template/d.rs
@@ -10,8 +10,8 @@ use std::cmp::Reverse;
 use std::collections::{BinaryHeap, HashMap, HashSet};
 use std::{char, f32, f64, i32, i64, usize};
 
-const inf_i: i64 = i64::MAX / 10;
-const inf_u: usize = usize::MAX / 10;
+const inf_i: i64 = (i64::MAX / 10) * 9;
+const inf_u: usize = (usize::MAX / 10) * 9;
 const md1000000007: i64 = 1_000_000_007;
 const md998244353: i64 = 998244353;
 const eps: f64 = 1e-10;
